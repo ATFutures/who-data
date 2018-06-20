4d98f68bb3289ba9a55d398fae348dead11392a1
mpadge <mark.padgham@email.com>
2018-06-20 17:01:27 +0200

    add up/download_popdens_nodes fns

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,32 @@ who_cities <- function ()
     c ("accra", "bristol", "kathmandu")
 }
 
+#' upload_popdens_nodes
+#' @export
+upload_popdens_nodes <- function ()
+{
+    flist <- file.path (c ("accra", "kathmandu"), "osm", "popdens_nodes.Rds")
+    junk <- lapply (flist, function (i)
+                    {
+                        message ("uploading ", i)
+                        piggyback::pb_upload (i, repo = "ATFutures/who-data",
+                                      tag = "v0.0.3-osmdata")
+                    })
+}
+
+#' download_popdens_nodes
+#' @export
+download_popdens_nodes <- function ()
+{
+    flist <- file.path (c ("accra", "kathmandu"), "osm", "popdens_nodes.Rds")
+    junk <- lapply (flist, function (i)
+                    {
+                        message ("downloading ", i)
+                        piggyback::pb_download (i, repo = "ATFutures/who-data",
+                                                tag = "v0.0.3-osmdata")
+                    })
+}
+
 #' download_who_data
 #'
 #' Download all WHO data from github repo via \pkg{piggyback}. This function can
