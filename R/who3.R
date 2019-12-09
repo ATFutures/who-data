d7c81534795129ddae8fc557209d659e9f57a034
mpadge <mark.padgham@email.com>
2019-12-09 12:32:01 +0100

    finish bus_centrality

--- R/who3.R
+++ R/who3.R
@@ -18,7 +18,7 @@ who3_bp <- function (city) {
     if (grepl ("accra", city, ignore.case = TRUE)) {
         #city <- "Greater Accra Region"
         bnames <- c ("Accra Metropolitan", "Ga West", "Ga East",
-                     "Ashaiman", "Adenta", "Ledzokuku-Krowor") 
+                     "Ashaiman", "Adenta", "Ledzokuku-Krowor")
         res <- lapply (bnames, function (i)
                        osmdata::getbb(place_name = i, format_out = "polygon"))
         # names which return multiple polygons:
@@ -53,7 +53,7 @@ who3_bp <- function (city) {
 #' @param quiet If `FALSE`, display progress information on screen.
 #' @return SC-formatted street network for nominated city.
 #' @export
-who3_network <- function (city, save = FALSE, quiet = FALSE) {
+who3_network <- function (city, save = TRUE, quiet = FALSE) {
     bp <- who3_bp (city)
 
     hw <- osmdata::opq (bbox = city) %>%
@@ -90,7 +90,7 @@ who3_network <- function (city, save = FALSE, quiet = FALSE) {
 #' @return A \pkg{dodgr} representation of the street network with a centrality
 #' column calculated from time-based weighting for motorcar transport.
 #' @export
-who3_centrality <- function (city, save = FALSE, quiet = FALSE) {
+who3_centrality <- function (city, save = TRUE, quiet = FALSE) {
     city <- tolower (city)
     f <- file.path (city, "osm", paste0 (city, "-hw-sc.Rds"))
     if (!file.exists (f))
@@ -113,7 +113,7 @@ who3_centrality <- function (city, save = FALSE, quiet = FALSE) {
         netc <- dodgr::dodgr_centrality (netc)
     )
     if (!quiet)
-        message ("\rCalculated centrality in ", 
+        message ("\rCalculated centrality in ",
                  as.numeric (st [3]), " seconds")
     net <- dodgr::dodgr_uncontract_graph (netc)
 
@@ -130,7 +130,7 @@ who3_centrality <- function (city, save = FALSE, quiet = FALSE) {
 #'
 #' @inheritParams who3_network
 #' @export
-who3_building <- function (city, save = FALSE, quiet = FALSE) {
+who3_building <- function (city, save = TRUE, quiet = FALSE) {
     bp <- who3_bp (city)
     bldg <- osmdata::opq (bbox = city) %>%
         osmdata::add_osm_feature (key = "building", value = "!residential") %>%
@@ -144,18 +144,57 @@ who3_building <- function (city, save = FALSE, quiet = FALSE) {
     return (bldg)
 }
 
-#' who3_bus_data
+#' who3_bus_network
 #'
-#' Get bus stop and bus network data for WHO3 cities (Accra, Kathmandu).
+#' Get bus network data for WHO3 cities (Accra, Kathmandu).
 #' @inheritParams who3_network
 #' @export
-who3_bus_data <- function (city, save = FALSE) {
+who3_bus_network <- function (city) {
+    city <- tolower (city)
+    f <- file.path (city, "osm", paste0 (city, "-bus-net.Rds"))
+    savefile <- TRUE
+
+    if (file.exists (f)) {
+        savefile <- FALSE
+        net <- readRDS (f)
+    } else {
+        if (grepl ("accra", city, ignore.case = TRUE)) {
+            net <- osmdata::opq ("accra ghana") %>%
+                osmdata::add_osm_feature (key = "bus") %>%
+                osmdata::add_osm_feature (key = "type", value = "route") %>%
+                osmdata::osmdata_sc (quiet = TRUE)
+        } else if (grepl ("kathmandu", city, ignore.case = TRUE)) {
+            x <- osmdata::opq ("kathmandu") %>%
+                osmdata::add_osm_feature (key = "route", value = "bus") %>%
+                osmdata::osmdata_sf (quiet = TRUE) %>%
+                osmdata::osm_poly2line ()
+            l <- x$osm_multilines
+            # then extract all constituent lines
+            ids <- do.call (c, lapply (l$geometry, function (i) names (i))) %>%
+                unname () %>%
+                unique ()
+            net <- x$osm_lines [x$osm_lines$osm_id %in% ids, ]
+        }
+    }
+
+    if (savefile)
+        saveRDS (net, f)
+
+    return (net)
+}
+
+#' who3_bus_stops
+#'
+#' Get bus stop data for WHO3 cities (Accra, Kathmandu).
+#' @inheritParams who3_network
+#' @export
+who3_bus_stops <- function (city, save = TRUE) {
     # Accra includes the bus stops within the network, Kathmandu does not
     if (grepl ("accra", city, ignore.case = TRUE)) {
-        net <- osmdata::opq ("accra ghana") %>%
-            osmdata::add_osm_feature (key = "bus") %>%
-            osmdata::add_osm_feature (key = "type", value = "route") %>%
-            osmdata::osmdata_sc (quiet = TRUE)
+        f <- file.path ("accra", "osm", "accra-bus-net.Rds")
+        if (!file.exists (f))
+            net <- who3_bus_network (city = "accra")
+        net <- readRDS (f)
 
         bus_stops <- net$relation_members %>%
             dplyr::filter (type == "node" & role == "platform")
@@ -163,8 +202,9 @@ who3_bus_data <- function (city, save = FALSE) {
         xy <- net$vertex [net$vertex$vertex_ %in% bus_stops, ]
         xy <- sf::st_multipoint (as.matrix (xy [, 1:2])) %>%
             sf::st_sfc (crs = 4326) %>%
-            st_cast ("POINT")
-        bus_stops <- sf::st_sf (id = bus_stops, geometry = xy)
+            sf::st_cast ("POINT")
+        bus_stops <- sf::st_sf (id = bus_stops, geometry = xy,
+                                stringsAsFactors = FALSE)
 
         # remove stops beyond the bounding polygon:
         p <- who3_bp ("Accra")
@@ -198,3 +238,73 @@ who3_bus_data <- function (city, save = FALSE) {
     return (bus_stops)
 }
 
+#' who3_bus_centrality
+#'
+#' Calculate centrality of bus networks for WHO3 cities, and assign vertex
+#' centrality values to each bus stop, in order to use these as proxies for
+#' numbers of passengers.
+#'
+#' @inheritParams who3_network
+#' @export
+who3_bus_centrality <- function (city) {
+    city <- tolower (city)
+    f <- file.path (city, "flows", paste0 (city, "-bus.Rds"))
+    if (!file.exists (f)) {
+        message ("Centrality file [", f, "] does not exist;\n",
+                 "calculating centrality ... ", appendLF = FALSE)
+        bus <- who3_bus_centrality_internal (city)
+        message ("Calculating centrality ... done")
+        saveRDS (bus, f)
+    } else
+        bus <- readRDS (f)
+    return (bus)
+}
+
+who3_bus_centrality_internal <- function (city) {
+    net <- who3_bus_network (city)
+    bus_stops <- who3_bus_stops (city)
+    dodgr::dodgr_cache_off ()
+    net <- dodgr::weight_streetnet (net, wt_profile = 1)
+    # re-map bus stop vertices to network:
+    v <- dodgr::dodgr_vertices (net)
+    index <- dodgr::match_points_to_graph (v, sf::st_coordinates (bus_stops))
+    bus_stops$id <- v$id [index]
+
+    flowmat <- array (1, dim = rep (nrow (bus_stops), 2))
+    netc <- dodgr::dodgr_contract_graph (net, verts = bus_stops$id)
+    netc <- dodgr::dodgr_flows_aggregate (netc, from = bus_stops$id,
+                                          to = bus_stops$id,
+                                          flows = flowmat, norm_sums = TRUE)
+    netm <- dodgr::merge_directed_graph (netc)
+
+    if (grepl ("kathmandu", city, ignore.case = TRUE)) {
+        # rename sf-network column names to sc-style
+        netm <- dplyr::rename (netm, .vx0 = from_id, .vx1 = to_id)
+    }
+
+    # then convert edge centrality to values at actual bus stops
+    out_flow <- netm %>%
+        dplyr::select (.vx0, flow) %>%
+        dplyr::rename (out_flow = flow, id = .vx0) %>%
+        dplyr::group_by (id) %>%
+        dplyr::summarise (out_flow = sum (out_flow))
+    in_flow <- netm %>%
+        dplyr::select (.vx1, flow) %>%
+        dplyr::rename (in_flow = flow, id = .vx1) %>%
+        dplyr::group_by (id) %>%
+        dplyr::summarise (in_flow = sum (in_flow))
+    out_flow <- tibble::tibble (id = bus_stops$id) %>%
+        dplyr::left_join (out_flow, by = "id") %>%
+        dplyr::filter (!is.na (out_flow)) %>%
+        dplyr::rename (flow = out_flow)
+    in_flow <- tibble::tibble (id = bus_stops$id) %>%
+        dplyr::left_join (in_flow, by = "id") %>%
+        dplyr::filter (!is.na (in_flow)) %>%
+        dplyr::rename (flow = in_flow)
+    # join both together:
+    bus <- dplyr::bind_rows (out_flow, in_flow) %>%
+        dplyr::group_by (id) %>%
+        dplyr::summarise (flow = sum (flow))
+
+    return (bus)
+}
